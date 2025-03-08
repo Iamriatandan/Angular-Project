e3cdba50a98f82fc74cd3e4f624a54867208867b
iamriatandan <tandanria@gmail.com>
2025-03-08 15:17:30 +0530

    Ria Added .

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,8 +10,14 @@ import { RouterOutlet } from '@angular/router';
 export class AppComponent {
   title = 'HelloWorld';
   imgUrl = "https://gumlet.assettype.com/freepressjournal%2F2021-06%2F32af7f9f-b603-4e12-8340-ba2425bb5ee8%2FBridgeLabz.PNG";
+  url = "https://www.bridgelabz.com";
 
   ngOnInit(): void{
     this.title = "Hello from Bridgelabz";
+  }
+
+  onClick($event: Event):void{
+    console.log("Save button is clicked!",$event);
+    window.open(this.url, "_blank");
   }
 }
